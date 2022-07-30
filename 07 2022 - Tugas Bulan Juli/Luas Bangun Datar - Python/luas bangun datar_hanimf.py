44022933af1ffad8635188a0fb2bfb74c20332cb
nipp <hanifmf01@gmail.com>
2022-07-30 13:47:44 +0700

    update

---
+++ 07 2022 - Tugas Bulan Juli/Luas Bangun Datar - Python/luas bangun datar_hanimf.py
@@ -0,0 +1,90 @@
+nama = input("Input your Name : ")
+nim = input("Input your NIM : ")
+print()
+
+print("................................")
+print("    Welcome to",nama,"          ")
+print("    With your NIM",nim,"        ")
+print("................................")
+print()
+
+#QUESTION NO 1
+print("Question No.1")
+print("Diketahui sisi = 6, sebutkan Nama dan Hitung Luas dari bangun datar tersebut!")
+
+nama_bangun_datar = input("Nama Bangun Datar : ")
+sisi1 = int(input("Sisi pertama : "))
+sisi2 = int(input("Sisi kedua : "))
+
+print("Bangun Datar tersebut adalah", nama_bangun_datar , "dan luasnya adalah : ",sisi1*sisi2)
+print()
+
+#QUESTION NO 2
+print("Question No.2")
+print("Diketahui alas = 7, tinggi = 8, hitung luas dari bangun datar tersebut!")
+
+print("Nama Bangun Datar : Segitiga")
+alas = int(input("Alas : "))
+tinggi = int(input("Tinggi : "))
+
+print("Bangun Datar tersebut adalah Segitiga dan luasnya adalah : ", alas*tinggi /2)
+print()
+
+#QUESTION NO 3
+print("Question No.3")
+print("Diketahui panjang = 4, lebar = 9, sebutkan nama hitung luas dari bangun datar tersebut!")
+
+nama_bangun_datar = input("Nama Bangun Datar : ")
+panjang = int(input("Panjang : "))
+lebar = int(input("Lebar : "))
+
+print("Bangun Datar tersebut adalah", nama_bangun_datar , "dan luasnya adalah : ", panjang*lebar)
+print()
+
+#QUESTION NO 4
+print("Question No.4")
+print("Diketahui Jari-jari = 56, sebutkan nama hitung luas dari bangun datar tersebut!")
+
+nama_bangun_datar = input("Nama Bangun Datar : ")
+π = float(input("π : "))
+r = float(input("r : "))
+
+print("Bangun Datar tersebut adalah", nama_bangun_datar , "dan luasnya adalah : ", π*r*r)
+print()
+
+#QUESTION NO 5
+print("Question No.5")
+print("Diketahui Diagonal I = 4, Diagonal II = 8 sebutkan nama hitung luas dari bangun datar tersebut!")
+
+nama_bangun_datar = input("Nama Bangun Datar : ")
+I = float(input("Diagonal I : "))
+II = float(input("Diagonal II : "))
+
+print("Bangun Datar tersebut adalah", nama_bangun_datar , "dan luasnya adalah : ", I*II/2)
+print()
+
+#QUESTION NO 6
+print("Question No.6")
+print("Diketahui sisi AB dan CD sejajar, sisi AB = 6, sisi CD = 8, tinggi = 3. Sebutkan nama hitung luas dari bangun datar tersebut!")
+
+nama_bangun_datar = input("Nama Bangun Datar : ")
+ab = float(input("Sisi AB : "))
+cd = float(input("Sisi CD : "))
+tinggi = float(input("Tinggi : "))
+print("Bangun Datar tersebut adalah", nama_bangun_datar , "dan luasnya adalah : ", (ab+cd)*tinggi/2 )
+print()
+
+#QUESTION NO 7
+print("Question No.7")
+print("Diketahui sebuah jajar genjang dengan alas = 9, tinggi = 7. Hitung luas dari bangun datar tersebut!")
+
+a = int(input("Alas : "))
+t = int(input("Tinggi : "))
+print("Luas jajar genjang adalah : ", a*t)
+print()
+
+print(".............................................")
+print(".                                           .")
+print(". 'Thankyou Very Much For Your Attention'   .")
+print(".                                           .")
+print(".............................................")
---
+++ 07 2022 - Tugas Bulan Juli/Tugas Python/Tugas Python_Hanif Miladi Fauzan.py
@@ -0,0 +1,34 @@
+print("hello world!")
+print("welcome to my website")
+
+nama = input("masukan nama anda : ")
+kelas = input("kelas : ")
+jurusan = input("jurusan : ")
+print("nama     : ",nama)
+print("kelas    : ",kelas)
+print("jurusan  : ",jurusan)
+
+print("berapakah nilai penjumlahan dari bilangan berikut, jika x = 250, y = 250, z = 250")
+x = eval(input("x = "))
+y = eval(input("y = "))
+z = eval(input("z = "))
+print("hasil dari penjumlahan x,y,z adalah", x + y + z)
+
+print("berapakah nilai pengurangan dari bilangan berikut, jika x = 255, y = 140, z = 30")
+x = eval(input("x = "))
+y = eval(input("y = "))
+z = eval(input("z = "))
+print("hasil dari pengurangan x,y,z adalah", x - y - z)
+
+print("berapakah nilai perkalian dari bilangan berikut, jika x = 2, y = 4, z = 5")
+x = eval(input("x = "))
+y = eval(input("y = "))
+z = eval(input("z = "))
+print("hasil dari perkalian x,y,z adalah", x * y * z)
+
+print("berapakah nilai pembagian dari bilangan berikut, jika x = 81, y = 9, z = 3")
+x = eval(input("x = "))
+y = eval(input("y = "))
+z = eval(input("z = "))
+print("hasil dari pembagian x,y,z adalah", x / y / z)
+
