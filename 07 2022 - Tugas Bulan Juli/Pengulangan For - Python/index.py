63806c6b25abf2352fc39da55bf8e1733effc5bd
nipp <hanifmf01@gmail.com>
2022-07-30 13:55:37 +0700

    update

---
+++ 07 2022 - Tugas Bulan Juli/Pengulangan For - Python/index.py
@@ -0,0 +1,90 @@
+# # menerima inputan
+# angka1 = int(input("masukkan angka pertama : "))
+# angka2 = int(5)
+
+# # menerima penjumlahan
+# hasil = angka1 + angka2
+
+# # menerima output
+# print(hasil)
+# hasil2 = (int("4") + hasil)
+# hasil2 = (4 + hasil)
+# print(int(hasil2))
+# print(float(hasil2))
+
+# angka = 5
+
+# for i in range(10):
+#     print(angka) 
+
+#membuat array mobil
+
+# mobil = ['honda jazz', 'avanza', 'kijang']
+# print(mobil)
+# print (mobil[1])
+
+# for indeks in mobil:
+#     print(indeks)
+    
+# for i in range(3):
+#     print(mobil(i))
+    
+# #membuat array buah buahan
+# buah = ['mangga', 'apel', 'anggur', 'jeruk', 'pisang', 'nanas', 'ceri']
+
+# #melakukan perulangan for
+# for buah_buahan in buah:
+#     #mencetak buah buahan
+#     print("nama buah : ", buah_buahan)
+    
+# #print manual
+# print("nama buah : ", buah[0])
+# print("nama buah : ", buah[1])
+# print("nama buah : ", buah[2])
+# print("nama buah : ", buah[3])
+# print("nama buah : ", buah[4])
+# print("nama buah : ", buah[5])
+# print("nama buah : ", buah[6])
+
+#membuat program biodata
+
+#membuat variabel nama_saya yang menerima inputan
+# nama_lengkap = input("masukkan nama lengkap : ")
+
+# #membuat variabel asal_daerah yang menerima inputan
+# asal_daerah = input("masukkan asal daerah : ")
+
+# #membuat variabel array biodata untuk mengumpulkan hasil inputan
+# biodata = [ nama_lengkap, asal_daerah ]
+
+# no = 0
+# for isi_biodata in biodata :
+#     print(biodata[no])
+#     no = no + 1
+
+# #membuat array siswa
+# siswa = []
+
+# #membuat perulangan for
+# for i in range(3):
+    
+#     #mengecek nilai indeks
+#     print("")
+#     print("ini adalah indeks ke-", i)
+    
+#     #menerima inputan nama
+#     nama_siswa = input("masukkan nama siswa : ")
+    
+#     #memasukan hasil inputan nama ke array siswa
+#     siswa.append(nama_siswa)
+    
+# print("")
+
+# #membuat perulangan untuk mencetak data dari array siswa
+# for k in siswa:
+    
+#     #mencetak array siswa
+#     print("nama siswa : " , k)
+
+while (1 + 2 == 4):
+    print('halo dunia!')
